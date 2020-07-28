1e8deeb6e899245f4d7b8798f1d1bf157b872f8d
Eslam Ayman <customeroragent@gmail.com>
2020-07-28 05:15:28 +0200

    first commit

---
+++ app/Http/Controllers/ImportController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class ImportController extends Controller
+{
+    public function import (Request $request)
+    {
+    	$request->validate([
+    		'file' => 'required|mimes:csv,xlsx'
+    	]);
+
+    	$extension = $request->file->getClientOriginalExtension();
+    	$file = file($request->file->getRealPath());
+    	$data = array_slice($file, 1);
+
+    	$parts =  (array_chunk($data, 5000));
+    	foreach ($parts as $key => $part) {
+    		$filename = storage_path('pending-files/'.date('y-m-d-H-i-s').$key.'.csv'/*.$extension*/);
+    		file_put_contents($filename, $part);
+    	}
+
+    	$this->importToDb($extension);
+    	return response(['status'=>'successfuly imported']);
+    }
+
+    public function importToDb($extension)
+    {
+    	$path = storage_path('pending-files/*'.'.'.'.csv'/*.$extension*/);
+
+    	$allFilesPaths = glob($path);
+
+    	foreach (array_slice($allFilesPaths, 0, 1) as $file) {
+    		$data = array_map('str_getcsv', file($file));
+    		dd($data);
+    		foreach ($data as $row) {
+    			User::create([
+    				'first_name' => $row[0],
+    				'second_name' => $row[1],
+    				'family_name' => $row[2],
+    				'UID' => $row[3],
+    			]);
+    		}
+
+    		unlink($file);
+    	}
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 191);
+            $table->string('second_name', 191);
+            $table->string('family_name', 191);
+            $table->string('UID', 191);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
